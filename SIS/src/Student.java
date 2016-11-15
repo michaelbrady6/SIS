bf6dd2e8e932272db6c6b43c83c8779336e94c15
michaelbrady6 <michael.brady108@gmail.com>
2016-11-15 09:40:44 -0700

    This is the updated version

--- SIS/src/Student.java
+++ SIS/src/Student.java
@@ -1,4 +1,15 @@
-
-public class Student {
-
-}
+public class Student
+	{
+		private String firstName;
+		private String lastName;
+		private String Class1;
+		private String Class2;
+		private String Class3;
+		private String Grade1;
+		private String Grade2;
+		private String Grade3;
+		public void Student()
+		{
+			
+		}
+	}
