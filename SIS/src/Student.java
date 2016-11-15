bc6d536e6c367e269ded8773f0600e7b5298ed68
BrandonA614 <brandon.ashkin@MU-C1-18.mullen.pvt.k12.co.us>
2016-11-15 10:32:33 -0700

    Init Branch Commit

--- SIS/src/Student.java
+++ SIS/src/Student.java
@@ -8,9 +8,15 @@ public class Student
 		private String Grade1;
 		private String Grade2;
 		private String Grade3;
-		private double GPA;
+		
+		
+		
 		public void Student()
 		{
+			
+			
+			
+			
 			
 		}
 	}
