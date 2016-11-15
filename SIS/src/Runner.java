43bc5dbe5f4443466c30d8e0e88e48e75098b725
michaelbrady6 <michael.brady108@gmail.com>
2016-11-15 10:33:28 -0700

    Adds first names to the ArrayList

--- SIS/src/Runner.java
+++ SIS/src/Runner.java
@@ -1,7 +1,52 @@
+import java.util.*;
+import java.io.*;
 public class Runner
 	{
-		public static void main(String[] args)
+		public static void main(String[] args)throws IOException
 			{
-				
+				ArrayList<Student> students = new ArrayList<Student>();
+				Scanner file = new Scanner(new File ("studentInformation.txt"));
+				int i = 0;
+				while(file.hasNextLine())
+					{
+						String line = file.nextLine();
+						students.add(new Student());
+						students.get(i).setFirstName(line.substring(0,line.indexOf(" ")));
+						i++;
+					}
+			}
+		public static void initSetup()
+			{
+				System.out.println("Welcome to INPUT NAME ");
+				System.out.println("Would you like to : ");
+				System.out.println(" (1) Add or Delete a Student");
+				System.out.println(" (2) Change Student grades");
+				System.out.println(" (3) Sort Students");
+				Scanner input = new Scanner(System.in);
+				int input2 = input.nextInt();
+
+				if (input2 == 1)
+					{
+						// Add how would you like students listed method
+						// Add or Delete Student method
+
+					} 
+				else if (input2 == 2)
+					{
+						// Add how would you like students listed method
+						// Change Student Grades method
+					} 
+				else if (input2 == 3)
+					{
+						// Add how would you like students listed method
+						// Sort Students method
+
+					} 
+				else
+					{
+						System.out.println("Please input Either 1 , 2 or,  3 ");
+
+					}
+
 			}
 	}
--- SIS/src/Student.java
+++ SIS/src/Student.java
@@ -10,7 +10,86 @@ public class Student
 		private String Grade3;
 		private double GPA;
 		public void Student()
-		{
-			
-		}
+			{
+				
+			}
+		@Override
+		public String toString()
+			{
+				return "Student [firstName=" + firstName + ", lastName=" + lastName + ", Class1=" + Class1 + ", Class2="
+						+ Class2 + ", Class3=" + Class3 + ", Grade1=" + Grade1 + ", Grade2=" + Grade2 + ", Grade3="
+						+ Grade3 + ", GPA=" + GPA + "]";
+			}
+		public String getFirstName()
+			{
+				return firstName;
+			}
+		public void setFirstName(String firstName)
+			{
+				this.firstName = firstName;
+			}
+		public String getLastName()
+			{
+				return lastName;
+			}
+		public void setLastName(String lastName)
+			{
+				this.lastName = lastName;
+			}
+		public String getClass1()
+			{
+				return Class1;
+			}
+		public void setClass1(String class1)
+			{
+				Class1 = class1;
+			}
+		public String getClass2()
+			{
+				return Class2;
+			}
+		public void setClass2(String class2)
+			{
+				Class2 = class2;
+			}
+		public String getClass3()
+			{
+				return Class3;
+			}
+		public void setClass3(String class3)
+			{
+				Class3 = class3;
+			}
+		public String getGrade1()
+			{
+				return Grade1;
+			}
+		public void setGrade1(String grade1)
+			{
+				Grade1 = grade1;
+			}
+		public String getGrade2()
+			{
+				return Grade2;
+			}
+		public void setGrade2(String grade2)
+			{
+				Grade2 = grade2;
+			}
+		public String getGrade3()
+			{
+				return Grade3;
+			}
+		public void setGrade3(String grade3)
+			{
+				Grade3 = grade3;
+			}
+		public double getGPA()
+			{
+				return GPA;
+			}
+		public void setGPA(double gPA)
+			{
+				GPA = gPA;
+			}
 	}
