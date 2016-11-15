2c627d304d7512aeb59654bf67b133a3f463c569
brandon.ashkin <brandon.ashkin@MU-C1-18.mullen.pvt.k12.co.us>
2016-11-15 09:54:35 -0700

    Added initSetup method

---
+++ SIS/MainMenu.java
@@ -0,0 +1,43 @@
+import java.util.*
+public class MainMenu {
+
+	public static void main(String[] args) {
+		
+		initSetup();
+		
+		
+		
+
+	}
+	
+	public static void initSetup(){
+		System.out.println("Welcome to INPUT NAME ")
+		System.out.println("Would you like to : ")
+		System.out.println(" (1) Add or Delete a Student" );
+		System.out.println(" (2) Change Student grades" );
+		System.out.println(" (3) Sort Students" );
+		Scanner input = new Scanner(System.in);
+		int input2 = input.nextInt();
+		
+		if(input2 == 1){
+			// Add how would you like students listed method
+			// Add or Delete Student method
+			
+		}else if(input2 == 2){
+			// Add how would you like students listed method
+			// Change Student Grades method
+		}else if(input2 == 3){
+			// Add how would you like students listed method
+			// Sort Students method
+			
+		}else {
+			System.out.println("Please input Either 1 , 2 or,  3 ");
+			
+		}
+		
+		
+		
+		
+	}
+
+}
